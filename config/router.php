f1c6eb5f448396b171b7c8c93a4dbaf92f595a43
Demon Zhang <demon.zhang@samesamechina.com>
2016-05-11 18:19:43 +0800

    no message

--- config/router.php
+++ config/router.php
@@ -2,7 +2,7 @@
 
 $routers = array();
 $routers['/'] = array('CoachBundle\Site', 'index');
-$routers['/qrcode'] = array('CoachBundle\Api', 'qrcode');
+$routers['/scan'] = array('CoachBundle\Api', 'qrcode');
 $routers['/getdata'] = array('CoachBundle\Api', 'getdata');
 $routers['/callback'] = array('CoachBundle\Api', 'callback');
 $routers['/api/test'] = array('CoachBundle\Api', 'test');
