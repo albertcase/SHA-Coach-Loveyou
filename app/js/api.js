9a05799734020ffa4a8ca4ead258c595ce8be6cb
evelynzhang88 <1054982653@qq.com>
2016-05-13 15:39:05 +0800

    follow

--- app/js/api.js
+++ app/js/api.js
@@ -2,19 +2,14 @@
 Api = {
     //api/status
     isFollow:function(callback){
-        //$.ajax({
-        //    url:'/api/status',
-        //    type:'POST',
-        //    dataType:'json',
-        //    success:function(data){
-        //        return callback(data);
-        //    }
-        //});
-        return callback(
-            {
-                status:'1'
+        $.ajax({
+            url:'/api/status',
+            type:'POST',
+            dataType:'json',
+            success:function(data){
+                return callback(data);
             }
-        );
+        });
     }
     ,
     //{"status":1,"msg":{"id":"1","openid":"oKCDxjivJ92ky4dxLT8dt1jcXtn4","nickname":"nickname","headimgurl":"headimgurl","greeting":false,"background":false,"ballot":"1”}}
@@ -44,7 +39,7 @@ Api = {
             type:'POST',
             dataType:'json',
             success:function(data){
-                callback(data);
+                return callback(data);
             }
         });
     },
